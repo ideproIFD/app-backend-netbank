f77df7ce291770cfdbcb5426a46f7597b8900984
marcial08 <aruquipamarcial08@gmail.com>
2022-11-09 17:28:01 -0400

    ADD CONFIGURACION WS AXON LABO-PROD

--- src/main/java/com/idepro/appbackendnetbank/util/ConstantsUtil.java
+++ src/main/java/com/idepro/appbackendnetbank/util/ConstantsUtil.java
@@ -132,8 +132,7 @@ public final class ConstantsUtil {
     public static final String PARAM_P_PRECIO = "PRECIO";
     public static final String PARAM_P_NO_CUMPLE = "NO CUMPLE";
     public static final String PARAM_P_CUMPLE = "CUMPLE";
-
-      public static final String PARAM_IP_WS_AXON = "http://services-labo-netbank.idepro.org/WSClientAxon/";
+    public static final String PARAM_IP_WS_AXON = "http://10.0.1.70/wsIdeproAxonWs/WSClientAxon/";
     // public static final String PARAM_IP_WS_AXON = "http://10.0.1.49/wsIdeproAxonWs/WSClientAxon/";
     public static final String PARAM_WS_AXON_PROXIMA_CUOTA = "proximaCuotaCredito/";
     public static final String PARAM_WS_PAGO_CREDITO = "PagoCredito/";
